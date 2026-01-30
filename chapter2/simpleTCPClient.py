6afc3d79b859dcca7b6d3b066ae8d9cbaef53be9
Umi <umi.oba28@gmail.com>
2026-01-29 21:59:56 -0800

    Created chapter 2 folder for chapter 2 content
    Created and tested a simple TCP client

---
+++ chapter2/simpleTCPClient.py
@@ -0,0 +1,31 @@
+"""
+A simple TCP Client example from chapter 2
+Some assumptions made to look out for:
+Connection always succeeds
+Server expects client to send code first
+Server always return data (in a timely fashion)
+"""
+
+
+import socket 
+target_host = "www.google.com"
+target_port = 80
+
+#create a sockcet object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect the client
+client.connect((target_host,target_port))
+
+#send some data
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+#receive some data
+response = client.recv(4096)
+
+print(response.decode())
+
+client.close()
+
+
+
